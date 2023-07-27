6d49280fe1b61a510c46e170a28b725a27cf5e9e
ray21011 <137093132+ray21011@users.noreply.github.com>
2023-07-27 15:54:52 +0000

    [Add]footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  
 root to: 'homes#top'
 get "homes/about" => "homes#about",as: "about"
   devise_for :users
+  
+  resources :books, only: [:new, :index, :show, :edit]
   resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
