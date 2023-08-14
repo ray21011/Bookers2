d5d143fead2050988ab28b197ad772ad170d3bc9
ray21011 <137093132+ray21011@users.noreply.github.com>
2023-08-14 07:17:42 +0000

    [Finish]Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about",as: "about"
   devise_for :users
 
-  resources :books, only: [:new, :index, :edit, :create, :show, :update, :destroy]
-  #post 'books' => 'books#create'
-  #get 'books/:id' => 'books#show', as: 'book'
-  #patch 'books/:id' => 'books#update', as: 'update_book'
-  #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  resources :users, only: [:show, :edit, :index, :update]
-  #patch 'users/:id' => 'users#update', as: 'update_user'
+  resources :books, only: [:new, :index, :edit]
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :users, only: [:show, :edit, :index]
+  patch 'users/:id' => 'users#update', as: 'update_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
